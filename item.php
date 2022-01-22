e52f7492e6ef8c52a08789ad560a117293497280
phan111 <chatpimookt@gmail.com>
2022-01-22 20:45:41 +0700

    css

--- item.php
+++ item.php
@@ -1,7 +1,6 @@
 <body>
 
     <head>
-        <meta name='viewport'content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'/><meta name="HandheldFriendly"content="true"/>
         <link rel="stylesheet" href="styles.css">
     </head>
     <?php
